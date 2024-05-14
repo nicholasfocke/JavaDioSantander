f4da2b0a4ca3776dfed885b65e177f012559d4b1
nicholasfocke <128650811+nicholasfocke@users.noreply.github.com>
2024-05-14 16:20:33 -0300

    java-anatomia-classes

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,18 @@
 public class MinhaClasse {
     
     public static void main (String []args){
-        System.out.print("Olá mundo!");
+        
+        String primeiroNome = "Nicholas";
+        String segundoNome = "Focke";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        
+        return "Resultado do metódo: " + primeiroNome.concat(" ").concat(segundoNome); 
+    
     }
 }
